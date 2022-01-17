6aeb3b398631b024db58d8079653943bf4da5fb6
Sergio99778 <sergio4brandon@gmail.com>
2022-01-17 01:28:52 -0600

    Adding ListImages and Image to App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from "react";
 
 //Components
 import Form from "./components/Form";
+import ListImages from "./components/ListImages";
 
 function App() {
   const [term, setTerm] = useState("");
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       if (term === "") return;
 
-      const imgPerPage = 30;
+      const imgPerPage = 10;
       const key = import.meta.env.VITE_KEY;
       const url = `https://pixabay.com/api/?key=${key}&q=${term}&per_page=${imgPerPage}`;
 
       const res = await fetch(url);
       const data = await res.json();
 
-      console.log(data.hits);
+      setImages(data.hits);
     };
     getData();
   }, [term]);
@@ -27,6 +29,9 @@ function App() {
       <div className="jumbotron">
         <p className="lead text-center">Images:</p>
         <Form setTerm={setTerm} />
+      </div>
+      <div className="row justify-content-center">
+        <ListImages images={images} />
       </div>
     </div>
   );
---
+++ src/components/ListImages.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Components
+import Image from "./Image";
+
+const ListImages = ({ images }) => {
+  return (
+    <div className="col-12 p-5 row">
+      {images.map((image) => (
+        <Image key={image.id} image={image} />
+      ))}
+    </div>
+  );
+};
+
+export default ListImages;
