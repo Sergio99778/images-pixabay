7079811dc9b1a84e424c95ca3040d0fea3c588cc
Sergio99778 <sergio4brandon@gmail.com>
2022-01-17 02:12:28 -0600

    Adding paginator

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ListImages from "./components/ListImages";
 function App() {
   const [term, setTerm] = useState("");
   const [images, setImages] = useState([]);
+  const [actualPage, setActualPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const getData = async () => {
@@ -14,15 +16,33 @@ function App() {
 
       const imgPerPage = 10;
       const key = import.meta.env.VITE_KEY;
-      const url = `https://pixabay.com/api/?key=${key}&q=${term}&per_page=${imgPerPage}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${term}&per_page=${imgPerPage}&page=${actualPage}`;
 
       const res = await fetch(url);
       const data = await res.json();
 
       setImages(data.hits);
+
+      //calc total pages
+      const calcTotalPages = Math.ceil(data.totalHits / imgPerPage);
+      setTotalPages(calcTotalPages);
+      //Scroll up
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({ behavior: "smooth" });
     };
     getData();
-  }, [term]);
+  }, [term, actualPage]);
+
+  const previousPage = () => {
+    const newActualPage = actualPage - 1;
+    if (newActualPage === 0) return;
+    setActualPage(newActualPage);
+  };
+  const nextPage = () => {
+    const newActualPage = actualPage + 1;
+    if (newActualPage > totalPages) return;
+    setActualPage(newActualPage);
+  };
 
   return (
     <div className="container">
@@ -30,8 +50,24 @@ function App() {
         <p className="lead text-center">Images:</p>
         <Form setTerm={setTerm} />
       </div>
-      <div className="row justify-content-center">
+      <div className="row justify-content-center ">
         <ListImages images={images} />
+        <button
+          type="button"
+          className="btn btn-info mr-1"
+          onClick={previousPage}
+          disabled={actualPage === 1}
+        >
+          &laquo; Previous
+        </button>
+        <button
+          type="button"
+          className="btn btn-info mr-1 "
+          onClick={nextPage}
+          disabled={actualPage === totalPages}
+        >
+          Next &raquo;
+        </button>
       </div>
     </div>
   );
